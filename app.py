8ae27f6d123e39ecc4932c593d4cfb19662a26a9
Michal Radziwon <michal@isysnet.pl>
2019-03-01 21:57:40 +0100

    removed dummy function

--- app.py
+++ app.py
@@ -22,8 +22,3 @@ if __name__ == '__main__':
    app.run(debug = True, port=5000, host='0.0.0.0')
 
    from flask import request
-
-@app.route(...)
-def login():
-    
-    password = request.args.get('password')
