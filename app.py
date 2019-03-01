a9723d4541d97ccb7c5e683f5f9d10634eaac9aa
Michal Radziwon <michal@isysnet.pl>
2019-03-01 21:50:13 +0100

    dodany interface do wesji sel i relay

--- app.py
+++ app.py
@@ -5,7 +5,17 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/version')
+def return_version():
+    return('0.0.1')
 
+@app.route('/sel')
+def select_relay():
+    return('SelectRelay')
+
+@app.route('/relay<number>')
+def set_relay(number):
+    return('relay'+str(numner))
 
 if __name__ == '__main__':
    app.run(debug = True, port=5000, host='0.0.0.0')
