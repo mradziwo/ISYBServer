7292552d16ec9e26f626200cf8e191e7b5aa0171
Michal Radziwon <michal@isysnet.pl>
2019-03-01 21:51:59 +0100

    fixed number typo

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def select_relay():
 
 @app.route('/relay<number>')
 def set_relay(number):
-    return('relay'+str(numner))
+    return('relay'+str(number))
 
 if __name__ == '__main__':
    app.run(debug = True, port=5000, host='0.0.0.0')
